79ab7452c2c6b8117508d3bf5bf33900c8f7e2b5
Tobias Wylega <tobias.wylega@stud.hs-hannover.de>
2023-07-16 12:41:18 +0200

    water prefab

---
+++ expoplazaAR/Assets/Shader/Water/Water.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Internal;
+using UnityEditor;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    ParticleSystem[] particleSystems;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get all children particle systems
+        particleSystems = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    public void Play()
+    {
+        foreach (ParticleSystem particleSystemObj in particleSystems)
+        {
+            particleSystemObj.Play();
+        }
+    }
+}
+
+[CustomEditor(typeof(Water))]
+public class TestEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Water test = (Water)target;
+        if (GUILayout.Button("Play Water Animation"))
+        {
+            test.Play();
+        }
+    }
+}

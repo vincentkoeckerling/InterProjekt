0db2f098be0c9649d14970cfcb5cd6e604698ccf
Tobias Wylega <tobias.wylega@stud.hs-hannover.de>
2023-07-17 12:52:01 +0200

    cleanup

--- expoplazaAR/Assets/Scripts/FishingGame/FishingSpot.cs
+++ expoplazaAR/Assets/Scripts/FishingGame/FishingSpot.cs
@@ -18,10 +18,9 @@ public class FishingSpot : MonoBehaviour
 
     private Vector3 GetRandomPositionOnMesh(Mesh mesh1)
     {
-        Vector3 pos = Vector3.zero;
         Vector3 randomPointOnMesh = mesh1.vertices[UnityEngine.Random.Range(0, mesh1.vertices.Length)];
         
-        pos = new Vector3(
+        Vector3 pos = new(
             randomPointOnMesh.x * meshRenderer.transform.localScale.x + meshRenderer.transform.position.x,
             randomPointOnMesh.y * meshRenderer.transform.localScale.y + meshRenderer.transform.position.y,
             randomPointOnMesh.z * meshRenderer.transform.localScale.z + meshRenderer.transform.position.z);

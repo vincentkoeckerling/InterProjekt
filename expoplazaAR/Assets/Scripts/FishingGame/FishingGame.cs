d9d61bf4cc1ce10410fa581d17216a6ebe0d19c6
Tobias Wylega <tobias.wylega@stud.hs-hannover.de>
2023-07-17 12:35:56 +0200

    spawn cube randomly on the lake

--- expoplazaAR/Assets/Scripts/FishingGame/FishingGame.cs
+++ expoplazaAR/Assets/Scripts/FishingGame/FishingGame.cs
@@ -26,7 +26,6 @@ public class FishingGame : MonoBehaviour
         if(_audioSource == null)
             Debug.LogError("No audio source found");
         
-        
         StartCoroutine(SpawnFish());
     }
 
---
+++ expoplazaAR/Assets/Scripts/FishingGame/FishingSpot.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingSpot : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public GameObject moveAround;
+
+    private Mesh _mesh;
+    
+    void Start()
+    {
+        _mesh = meshRenderer.GetComponent<MeshFilter>().mesh;
+    }
+
+
+    private Vector3 GetRandomPositionOnMesh(Mesh mesh1)
+    {
+        Debug.Log(mesh1.vertices.Length);
+        Vector3 pos = Vector3.zero;
+        var randomPointOnMesh = mesh1.vertices[UnityEngine.Random.Range(0, mesh1.vertices.Length)] * 2000;
+        pos = randomPointOnMesh;
+        
+        return pos;
+    }
+
+    private void FixedUpdate()
+    {
+        moveAround.transform.position = GetRandomPositionOnMesh(_mesh);
+    }
+}

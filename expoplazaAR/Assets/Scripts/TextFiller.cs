c79cf99b65fd0b8fc1b1ae408437d00b51c43859
Vincent Köckerling <v.koeckerling@icloud.com>
2023-07-19 00:21:44 +0200

    Text format

--- expoplazaAR/Assets/Scripts/TextFiller.cs
+++ expoplazaAR/Assets/Scripts/TextFiller.cs
@@ -8,28 +8,35 @@ using ScriptableObjects;
 public class TextFiller : MonoBehaviour
 {
 
-    public TMP_Text infoText;
+	public TMP_Text infoText;
 
-    public UserInfo userInfo;
-    // Start is called before the first frame update
-    void Start()
-    {
-        infoText = GetComponent<TMP_Text>();
-    }
+	public UserInfo userInfo;
+	// Start is called before the first frame update
+	void Start()
+	{
+		infoText = GetComponent<TMP_Text>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        String formatedText = $"Username : {userInfo.username} \n" +
-                              $"Email : {userInfo.email} \n" + 
-                              $"Password : {userInfo.password}\n" +
-                              $"Vorname : {userInfo.firstName}\n" +
-                              $"Nachname : {userInfo.lastName}\n" +
-                              $"Geschecht : {userInfo.gender}\n" +
-                              $"Geburtsdatum {userInfo.birthday}\n" +
-                              $"Adresse : {userInfo.street}\n" +
-                              $"{userInfo.zipCode}\n"+
-                              $"{userInfo.city}";
-        infoText.text = formatedText;    
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		String additionalInfo = "";
+		foreach (var str in userInfo.additionalInfo)
+		{
+			additionalInfo += $"  - {str}\n";
+		}
+
+		String formatedText = $"Username :\t{userInfo.username} \n" +
+									 $"Email :\t\t{userInfo.email} \n" +
+									 $"Password :\t{userInfo.password}\n" +
+									 $"Vorname :\t{userInfo.firstName}\n" +
+									 $"Nachname :\t{userInfo.lastName}\n" +
+									 $"Geschlecht :\t{userInfo.gender}\n" +
+									 $"Geburtsdatum :\t{userInfo.birthday}\n" +
+									 $"Adresse :\t{userInfo.street}\n" +
+									 $"\t\t{userInfo.zipCode} {userInfo.city}\n" +
+									 $"Weitere Informationen :\n" +
+									 additionalInfo;
+		infoText.text = formatedText;
+	}
 }

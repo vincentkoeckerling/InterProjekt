b11ca8d52412505965050d000c891c11adae5f27
Vincent Köckerling <v.koeckerling@icloud.com>
2023-07-18 23:08:03 +0200

    Stabilerer Lake

--- expoplazaAR/Assets/Scripts/Helper/CameraHelper.cs
+++ expoplazaAR/Assets/Scripts/Helper/CameraHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.IO;
 using UnityEngine;
+using Unity.XR.CoreUtils;
 
 namespace Helper
 {
@@ -18,6 +19,9 @@ namespace Helper
 
 		public static IEnumerator TakePhoto()  // Start this Coroutine on some button click
 		{
+			var camPos = Camera.main.transform.position;
+			var camForward = Camera.main.transform.forward;
+
 			WebCamTexture webCamTexture = new(GetFrontCamName());
 
 			webCamTexture.Play();
@@ -27,15 +31,15 @@ namespace Helper
 			photo.SetPixels(webCamTexture.GetPixels());
 			photo.Apply();
 
-            byte[] bytes = photo.EncodeToPNG();
-            string filename =
-                $"{Application.productName}_Capture{{0}}_{System.DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png";
-            NativeGallery.SaveImageToGallery(bytes, Application.productName + " Captures", filename);
-		
-		
-            DirectoryInfo directoryInfo = Directory.CreateDirectory(Application.persistentDataPath).CreateSubdirectory("ARFishing");
-            string destination = directoryInfo.FullName + filename;
-            Debug.Log("save destination: " + destination); 
+			byte[] bytes = photo.EncodeToPNG();
+			string filename =
+				 $"{Application.productName}_Capture{{0}}_{System.DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png";
+			NativeGallery.SaveImageToGallery(bytes, Application.productName + " Captures", filename);
+
+
+			DirectoryInfo directoryInfo = Directory.CreateDirectory(Application.persistentDataPath).CreateSubdirectory("ARFishing");
+			string destination = directoryInfo.FullName + filename;
+			Debug.Log("save destination: " + destination);
 
 
 			FileStream file = File.Exists(destination) ? File.OpenWrite(destination) : File.Create(destination);
@@ -43,6 +47,12 @@ namespace Helper
 			file.Close();
 
 			webCamTexture.Stop();
+			yield return new WaitForSeconds(0.1f);
+			Debug.Log(Camera.main.transform.position);
+			var xrorigin = GameObject.FindWithTag("XROrigin").GetComponent<XROrigin>();
+			xrorigin.MoveCameraToWorldLocation(camPos);
+			xrorigin.MatchOriginUpCameraForward(xrorigin.transform.up, camForward);
+			// Camera.main.transform.position = camPos;
 		}
 	}
 }

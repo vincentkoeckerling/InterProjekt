2e2b2b60590c5ff5d044d3d8724fdaba70593623
Tobias Wylega <tobias@wylega.de>
2023-07-18 16:52:56 +0200

    cheap trigger replacement

--- expoplazaAR/Assets/Scripts/OnTriggerSwitchScene.cs
+++ expoplazaAR/Assets/Scripts/OnTriggerSwitchScene.cs
@@ -7,7 +7,18 @@ using UnityEngine.SceneManagement;
 public class OnTriggerSwitchScene : MonoBehaviour
 {
     public String sceneName;
-    
+    public GameObject player;
+
+    private void Update()
+    {
+        // if distance between transform and player is less than 1.5f
+        if (Vector3.Distance(transform.position, player.transform.position) < 1.5f)
+        {
+            // Switch Scene
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
